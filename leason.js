d5f17afbb23936dab44b2cc03363f6e4280317d7
abuhasanmahmud <mahmud.me420@gmail.com>
2023-08-25 21:17:13 +0600

    typescript first leason

--- leason.js
+++ leason.js
@@ -1,5 +1,13 @@
 //leason 1
-function addNumbers(num1, num2) {
-    console.log(num1 + num2);
-}
-addNumbers(20, 30);
+// function addNumbers(num1: number, num2: number) {
+//   console.log(num1 + num2);
+// }
+// addNumbers(20, 30);
+//leason 2
+var userId = 20;
+var firstName = "abu hasan ";
+var lastName = "mahmud";
+var fullName = firstName.concat(lastName);
+// console.log(fullName.split(" "));
+var fulnameArray = firstName.split(" ");
+fulnameArray.map(function (name) { return console.log(name); });
--- leason.ts
+++ leason.ts
@@ -1,7 +1,20 @@
 //leason 1
 
-function addNumbers(num1: number, num2: number) {
-  console.log(num1 + num2);
-}
+// function addNumbers(num1: number, num2: number) {
+//   console.log(num1 + num2);
+// }
 
-addNumbers(20, 30);
+// addNumbers(20, 30);
+
+//leason 2
+
+let userId: number = 20;
+let firstName: string = "abu hasan ";
+let lastName: string = "mahmud";
+
+let fullName = firstName.concat(lastName);
+// console.log(fullName.split(" "));
+
+let fulnameArray = firstName.split(" ");
+
+fulnameArray.map((name) => console.log(name));
